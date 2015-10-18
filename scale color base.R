65e3817eb23b161a21157f45a6578b9d355f064a
Michael Koontz <mikoontz@gmail.com>
2015-10-18 13:01:35 -0700

    Merged new range functionality with default behavior
    Condensed range functionality so that all recasting operations use the
    range passed to the function (or the range of the value argument as a
    default)

--- scale color base.R
+++ scale color base.R
@@ -8,19 +8,14 @@
 ###
 ### Intention: Convenient wrapper function to make a color ramp in base R. Takes a vector of values, a character vector of colors to map to, an optional new numeric range to map to (useful for fixing a numeric range to make two separate vectors comparable), an optional na.rm, and a transparency option.
 
-scale_color_base <- function(value, colors=c("black", "white"), new_range=NA, na.rm=FALSE, alpha=1)
+scale_color_base <- function(value, colors=c("black", "white"), range=range(value, na.rm=TRUE), na.rm=FALSE, alpha=1)
 {
-  if (any(is.na(new_range))) {
-  # Values need to be between 0 and 1
-    recast_value <- (value-min(value, na.rm=na.rm)) / max(value-min(value, na.rm=na.rm), na.rm=na.rm)
-  }
-  else {
-    recast_value <- (value - new_range[1]) / (diff(new_range))
+
+    recast_value <- (value - range[1]) / (diff(range))
     recast_value[recast_value < 0] <- 0
     recast_value[recast_value > 1] <- 1
-    
-  }
-  # Use the colorRamp function to create a function that will parse values into a 3-column matrix of rgb fields. Specify the range that the colors should span (low to high)
+
+    # Use the colorRamp function to create a function that will parse values into a 3-column matrix of rgb fields. Specify the range that the colors should span (low to high)
   color_fnc <- colorRamp(colors=colors)
 
   # Define the plot colors by calling the rgb() function. Divide the 3-column matrix result of color.fnc by 255 such that values remain between 0 and 1.
@@ -47,6 +42,11 @@ plot(n, col=scale_color_base(n, colors=c("blue", "red")), pch=19)
 plot(n, col=scale_color_base(n, colors=c("blue", "purple", "red")), pch=19)
 
 # Recasting subtracts the minimum from all elements of the value vector and divides by the maximum of the frame-shifed vector. Use na.rm=TRUE if there are NAs.
-range(n)
 
-plot(n, col=scale_color_base(n, colors=c("blue", "purple", "red"), new_range=c(-50, 0)), pch=19)
+
+# Fix the numeric range to map the values to. Useful for comparing two vectors with the same color palette.
+par(mfrow=c(1,2))
+n <- 30:40
+plot(n, col=scale_color_base(n, colors=c("blue",  "red"), range=c(20, 40)), pch=19)
+n2 <- 20:30
+plot(n2, col=scale_color_base(n2, colors=c("blue", "red"), range=c(20, 40)), pch=19)
